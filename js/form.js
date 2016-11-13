45966d8297fd119f885474d52ef2ac7a51717a71
lucy-lucy <jackiexmelt@gmail.com>
2016-11-13 20:06:09 +0300

    Доработала интерактивные элементы, добавила спрайт

--- js/form.js
+++ js/form.js
@@ -1,5 +1,8 @@
 var link = document.querySelector(".js-link");
-var form = document.querySelector('.search-form');
+var form = document.querySelector(".search-form");
+
+var arrivalDate = document.querySelector("[name=arrival-date]");
+var departureDate = document.querySelector("[name=departute-date]");
 
 form.classList.add("search-form-hide");
 
@@ -7,4 +10,14 @@ link.addEventListener ("click", function(event) {
   event.preventDefault();
   form.classList.toggle("search-form-hide");
   form.classList.toggle("search-form-show");
+  arrivalDate.focus();
+});
+
+form.addEventListener("submit", function(event) {
+  if(!arrivalDate.value || departureDate.value) {
+    event.preventDefault();
+    form.classList.remove("search-form-error");
+    form.offsetWidth = form.offsetWidth;
+    form.classList.add("search-form-error");
+  }
 });
