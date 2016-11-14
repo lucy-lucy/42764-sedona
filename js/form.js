0b7738817e71b74e240c2990e7c30c791b686cb2
lucy-lucy <jackiexmelt@gmail.com>
2016-11-14 22:39:14 +0300

    Внесла исправления, провела базовую оптимизацию

--- js/form.js
+++ js/form.js
@@ -1,23 +1,27 @@
 var link = document.querySelector(".js-link");
 var form = document.querySelector(".search-form");
 
-var arrivalDate = document.querySelector("[name=arrival-date]");
-var departureDate = document.querySelector("[name=departute-date]");
+var arrivalDate = form.querySelector("[name=arrival-date]");
+var departureDate = form.querySelector("[name=departure-date]");
+var searchBtn = form.querySelector('.search-btn');
 
 form.classList.add("search-form-hide");
 
 link.addEventListener ("click", function(event) {
   event.preventDefault();
+  form.classList.remove("search-form-error");
   form.classList.toggle("search-form-hide");
   form.classList.toggle("search-form-show");
   arrivalDate.focus();
 });
 
 form.addEventListener("submit", function(event) {
-  if(!arrivalDate.value || departureDate.value) {
-    event.preventDefault();
-    form.classList.remove("search-form-error");
-    form.offsetWidth = form.offsetWidth;
-    form.classList.add("search-form-error");
+  if(form && searchBtn){
+    if(!arrivalDate.value || !departureDate.value) {
+      event.preventDefault();
+      form.classList.remove("search-form-error");
+      form.offsetWidth;
+      form.classList.add("search-form-error");
+    }
   }
 });
